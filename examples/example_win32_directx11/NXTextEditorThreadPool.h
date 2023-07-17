2c26f9193d54db4818d5bd65ab6dd27d42fddbf7
moso31 <xatian23@126.com>
2023-07-18 01:24:10 +0800

    ..

--- examples/example_win32_directx11/NXTextEditorThreadPool.h
+++ examples/example_win32_directx11/NXTextEditorThreadPool.h
@@ -23,23 +23,25 @@ public:
             thread = std::thread([this]() {
                 while (!m_bShutdown)
                 {
-                    // 同时只能有一个线程访问、修改任务队列
-                    m_mutex.lock();
-                    if (!m_tasks.empty())
+                    FuncType task;
                     {
-                        // 如果任务队列不为空，从中取出一个任务并执行
+                        // 同时只能有一个线程访问、修改任务队列
+                        std::unique_lock<std::mutex> lock(m_mutex);
+
+                        // 调用 wait 后可能会出现两种情况
+                        // lambda = true: 即task队列不为空。此时会保持上锁状态，lambda立即return，执行后续代码。
+                        // lambda = false: 即当前task队列为空。此时会解锁mutex（其它线程就可以进行锁相关的操作了），并自动阻塞当前线程，直到其它地方调用notify_one或notify_all才能重新唤醒它。
+                        //      note：在当前语境下，要调用notify_one或notify_all唤醒此线程之前，最好先确保task队列不为空，有任务可指派。不然wait会重新阻塞（详见AddTaskFunc）。
+                        m_condition.wait(lock, [this]() { return m_bShutdown || !m_tasks.empty(); });
+
+                        // 从任务队列中取出一个任务并执行。
                         FuncType task = std::move(m_tasks.front());
                         m_tasks.pop();
-                        m_mutex.unlock();
-                        task();
-                    }
-                    else
-                    {
-                        // 如果任务队列为空，睡眠100ms，再重新检查
-                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                        m_mutex.unlock();
-                    }
-                }
+                    } // 出作用域 会自动解锁。其它线程就可以进行锁相关操作了。
+
+                    // 然后就可以执行实际任务了
+                    task();
+                } 
             });
         }
     }
@@ -47,6 +49,8 @@ public:
     void Shutdown()
     {
         m_bShutdown = true;
+        // 唤醒所有线程，让它们退出循环
+        m_condition.notify_all();
         for (auto& thread : m_threads)
         {
             if (thread.joinable())
@@ -56,8 +60,14 @@ public:
 
     void AddTaskFunc(FuncType func)
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_tasks.push(func);
+        if (m_bShutdown) return;
+
+        m_mutex.lock();     // 先上锁
+        m_tasks.push(func); // 再修改任务队列
+        m_mutex.unlock();   // 修改完成后解锁
+
+        // 确保任务队列中有任务以后，再使用notify_one唤醒某个线程，执行wait之后的代码
+        m_condition.notify_one();
     }
 
     ~NXTextEditorThreadPool() { Shutdown(); }
@@ -66,6 +76,7 @@ private:
     std::vector<std::thread> m_threads;
     std::queue<FuncType> m_tasks;
 
+    std::condition_variable m_condition;
     std::mutex m_mutex;
 
     std::atomic_bool m_bShutdown = false;
